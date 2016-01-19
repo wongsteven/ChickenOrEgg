4f6a56055239cce321906a95cf95b61599dee2bf
Steven W <mr.wong.steven@gmail.com>
2016-01-19 16:25:25 -0500

    Initial Commit

---
+++ ChickenOrEgg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChickenOrEgg
+//
+//  Created by steven wong on 1/19/16.
+//  Copyright © 2016 AuCreations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
